a65bf88e3126e14c51a2201d09d753bec768d30a
Jorge Mora <jorgemorah671@gmail.com>
2025-02-13 10:18:18 -0600

    ci: :rocket: Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
