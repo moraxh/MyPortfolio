384dd7201daafddf862b943420dbe8bf7988027c
Jorge Mora <jorgemorah671@gmail.com>
2025-02-13 10:22:24 -0600

    fix: :bug: Fixes before vercel test deployment

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,8 +6,8 @@ const projects = defineCollection({
     description: z.string(),
     tech: z.array(z.string()),
     isProfessional: z.boolean(),
-    githubName: z.string().nullable(),
-    thumbnail: z.string().nullable()
+    githubName: z.string().optional(),
+    thumbnail: z.string().optional(),
   })
 })
 
