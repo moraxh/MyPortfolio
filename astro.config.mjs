500e36a8d6b78cfe6c0f62bc7918fcd6eb4c8bba
Jorge Mora <jorgemorah671@gmail.com>
2026-06-02 21:28:23 -0600

    fix: update path alias resolution in astro.config.mjs for improved portability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        "@": "/home/moraxh/Desktop/Personal/MyPortfolio/src",
+        "@": path.resolve(__dirname, "src"),
       },
     },
     plugins: [tailwindcss()],
